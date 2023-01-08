ce7e67e82a6774604f7350ac102c8edc8bb1233a
RenanGeorgio <regeo9@hotmail.com>
2023-01-08 19:53:04 -0300

    feat

--- app/src/main/java/com/ustglobal/arcloudanchors/MainActivity.java
+++ app/src/main/java/com/ustglobal/arcloudanchors/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,35 +15,29 @@ import android.widget.Toast;
 
 import com.google.ar.core.Anchor;
 import com.google.ar.core.ArCoreApk;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.TransformableNode;
+import com.supplyfy.helpers.helpers.FullScreenHelper;
+
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     private CustomArFragment arFragment;
-    private ArrayList anchorList;
-    public Spinner modelOptionsSpinner;
-    private static final String[] paths = {"Straight Arrow", "Right Arrow", "Left Arrow"};
+
     private String FROM, MODE;
 
-    private enum AppAnchorState {
+    public enum AppAnchorState {
         NONE,
         HOSTING,
         HOSTED
     }
-
-    private Anchor anchor;
-    private AnchorNode anchorNode;
-    private AppAnchorState appAnchorState = AppAnchorState.NONE;
-    private String ELECTRONICS = "electronics_DB";
-    private String TOYS = "toys_DB";
-    private String TV_APPLIANCES = "tv_DB";
-    private String CLOTHING = "clothing_DB";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,143 +52,42 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             }
         }
 
-        //ArCoreApk.Availability availability = ArCoreApk.getInstance().checkAvailability(this);
         setContentView(R.layout.activity_main);
-        anchorList = new ArrayList();
+
         TinyDB tinydb = new TinyDB(getApplicationContext());
-        Button resolve = findViewById(R.id.resolve);
-        modelOptionsSpinner = findViewById(R.id.modelOptions);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, paths);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        modelOptionsSpinner.setAdapter(adapter);
-        modelOptionsSpinner.setOnItemSelectedListener(this);
-
+        FragmentManager fm = getSupportFragmentManager();
         arFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
-        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
-            //Active only in Admin Mode
-            if(MODE.equalsIgnoreCase("admin")) {
-                Log.d("HIT_RESULT:", hitResult.toString());
-                anchor = arFragment.getArSceneView().getSession().hostCloudAnchor(hitResult.createAnchor());
-                appAnchorState = AppAnchorState.HOSTING;
-                showToast("Hosting...");
-                createCloudAnchorModel(anchor);
-            } else {
-                showToast("Anchor can be hosted only in Admin mode");
-            }
 
-        });
+        Bundle bundle01 = new Bundle();
+        bundle01.putParcelable("customFragment", (Parcelable) arFragment);
 
-        arFragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
+        Bundle bundle02 = new Bundle();
+        bundle02.putString("mode", MODE);
 
-            if (appAnchorState != AppAnchorState.HOSTING)
-                return;
-            Anchor.CloudAnchorState cloudAnchorState = anchor.getCloudAnchorState();
+        Bundle bundle03 = new Bundle();
+        bundle03.putParcelable("tinydb", (Parcelable) tinydb);
 
-            if (cloudAnchorState.isError()) {
-                showToast(cloudAnchorState.toString());
-            } else if (cloudAnchorState == Anchor.CloudAnchorState.SUCCESS) {
-                appAnchorState = AppAnchorState.HOSTED;
+        Bundle bundle04 = new Bundle();
+        bundle04.putString("from", FROM);
 
-                String anchorId = anchor.getCloudAnchorId();
-                anchorList.add(anchorId);
+        if(arFragment == null) {
+            Fragment frag = new CloudAnchorFragment();
 
-                if (FROM.equalsIgnoreCase(LauncherActivity.ELECTRONICS)) {
-                    tinydb.putListString(ELECTRONICS, anchorList);
-                } else if (FROM.equalsIgnoreCase(LauncherActivity.TOYS)) {
-                    tinydb.putListString(TOYS, anchorList);
-                } else if (FROM.equalsIgnoreCase(LauncherActivity.TV_APPLIANCES)) {
-                    tinydb.putListString(TV_APPLIANCES, anchorList);
-                } else if (FROM.equalsIgnoreCase(LauncherActivity.CLOTHING)) {
-                    tinydb.putListString(CLOTHING, anchorList);
-                }
+            arFragment = (CustomArFragment) frag;
 
-                showToast("Anchor hosted successfully. Anchor Id: " + anchorId);
-            }
+            arFragment.setArguments(bundle01);
+            arFragment.setArguments(bundle02);
+            arFragment.setArguments(bundle03);
+            arFragment.setArguments(bundle04);
 
-        });
-
-
-        resolve.setOnClickListener(view -> {
-            ArrayList<String> stringArrayList = new ArrayList<>();
-            if (FROM.equalsIgnoreCase(LauncherActivity.ELECTRONICS)) {
-                stringArrayList = tinydb.getListString(ELECTRONICS);
-            } else if (FROM.equalsIgnoreCase(LauncherActivity.TOYS)) {
-                stringArrayList = tinydb.getListString(TOYS);
-            } else if (FROM.equalsIgnoreCase(LauncherActivity.TV_APPLIANCES)) {
-                stringArrayList = tinydb.getListString(TV_APPLIANCES);
-            } else if (FROM.equalsIgnoreCase(LauncherActivity.CLOTHING)) {
-                stringArrayList = tinydb.getListString(CLOTHING);
-            }
-
-            for (int i = 0; i < stringArrayList.size(); i++) {
-                String anchorId = stringArrayList.get(i);
-                if (anchorId.equals("null")) {
-                    Toast.makeText(this, "No anchor Id found", Toast.LENGTH_LONG).show();
-                    return;
-                }
-
-                Anchor resolvedAnchor = arFragment.getArSceneView().getSession().resolveCloudAnchor(anchorId);
-                createCloudAnchorModel(resolvedAnchor);
-
-            }
-
-
-        });
-
-        if (MODE.equalsIgnoreCase("user")) {
-            modelOptionsSpinner.setVisibility(View.GONE);
-        } else {
-            modelOptionsSpinner.setVisibility(View.VISIBLE);
-            resolve.setVisibility(View.VISIBLE);
+            fm.beginTransaction().add(R.id.fragment, arFragment).commit();
         }
-
-
-    }
-
-    private void showToast(String s) {
-        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
-    }
-
-    private void createCloudAnchorModel(Anchor anchor) {
-        ModelRenderable
-                .builder()
-                .setSource(this, Uri.parse("model.sfb"))
-                .build()
-                .thenAccept(modelRenderable -> placeCloudAnchorModel(anchor, modelRenderable));
-
-    }
-
-    private void placeCloudAnchorModel(Anchor anchor, ModelRenderable modelRenderable) {
-        anchorNode = new AnchorNode(anchor);
-        /*AnchorNode cannot be zoomed in or moved
-        So we create a TransformableNode with AnchorNode as the parent*/
-        TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem());
-
-        if (modelOptionsSpinner.getSelectedItem().toString().equals("Straight Arrow")) {
-            transformableNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 1f, 0), 225));
-        }
-        if (modelOptionsSpinner.getSelectedItem().toString().equals("Right Arrow")) {
-            transformableNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 1f, 0), 135));
-        }
-        if (modelOptionsSpinner.getSelectedItem().toString().equals("Left Arrow")) {
-            transformableNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 1f, 0), 315));
-        }
-        transformableNode.setParent(anchorNode);
-        //adding the model to the transformable node
-        transformableNode.setRenderable(modelRenderable);
-        //adding this to the scene
-        arFragment.getArSceneView().getScene().addChild(anchorNode);
     }
 
     @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        FullScreenHelper.setFullScreenOnWindowFocusChanged(this, hasFocus);
     }
 }
